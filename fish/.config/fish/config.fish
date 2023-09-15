7209ff41bb4450bc79909608e397c6ddec2c77af
Ramy Tanios <ramy.tanios@gmail.com>
2023-09-15 10:24:46 +0200

    eza; aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,9 +4,18 @@ end
 
 fish_vi_key_bindings
 
+# vim
 alias vi "nvim"
+alias nvim "nvim"
+alias vim "nvim"
+alias v "nvim"
 
+# git
 alias gs "git status"
 alias ga "git add"
 alias gc "git commit"
 alias gps "git push"
+
+# ls
+alias la "eza -la --git --icons"
+alias l "eza -l --git --icons"
