89b22c3024d833afe8cca56c1b5aec856446baf4
Ramy Tanios <ramy.tanios@gmail.com>
2023-10-07 10:23:53 +0200

    conform and nvim lint

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,161 +6,169 @@ vim.g.mapleader = " " -- space leader keymap
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  "tpope/vim-eunuch",
-  "tpope/vim-fugitive",
-  "scalameta/nvim-metals",
-  "hrsh7th/nvim-cmp",
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-path",
-  "L3MON4D3/LuaSnip",
-  "saadparwaiz1/cmp_luasnip",
-  "rafamadriz/friendly-snippets",
-  "hrsh7th/cmp-nvim-lsp",
-  "onsails/lspkind.nvim",
-  "christoomey/vim-tmux-navigator",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "VeryLazy",
-    opts = {},
-    config = function(_, opts) require 'lsp_signature'.setup(opts) end
-  },
-  {
-    -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
-      -- Useful status updates for LSP
-      -- : `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim',       tag = 'legacy', opts = {}, event = "LspAttach" },
-      -- Additional lua configuration, makes nvim stuff amazing!
-      'folke/neodev.nvim',
-    },
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
-  {
-    "numToStr/Comment.nvim",
-    opts = {},
-    lazy = false,
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-      require("lualine").setup({ options = { theme = "tokyonight" } })
-    end,
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-  },
-  {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-  },
-  {
-    "xiyaowong/telescope-emoji.nvim",
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-    },
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        highlight = { enable = true },
-        indent = { enable = true },
-        auto_install = true,
-      })
-    end,
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-    end,
-  },
-  {
-    "stevearc/oil.nvim",
-    config = function()
-      require("oil").setup({ view_options = { show_hidden = true }
-      })
-    end,
-    -- Optional dependencies
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
-  {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
-  },
+	"tpope/vim-eunuch",
+	"tpope/vim-fugitive",
+	"scalameta/nvim-metals",
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip",
+	"rafamadriz/friendly-snippets",
+	"hrsh7th/cmp-nvim-lsp",
+	"onsails/lspkind.nvim",
+	"christoomey/vim-tmux-navigator",
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "VeryLazy",
+		opts = {},
+		config = function(_, opts)
+			require("lsp_signature").setup(opts)
+		end,
+	},
+	{
+		-- LSP Configuration & Plugins
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- Automatically install LSPs to stdpath for neovim
+			{ "williamboman/mason.nvim", config = true },
+			"williamboman/mason-lspconfig.nvim",
+			-- Useful status updates for LSP
+			-- : `opts = {}` is the same as calling `require('fidget').setup({})`
+			{ "j-hui/fidget.nvim", tag = "legacy", opts = {}, event = "LspAttach" },
+			-- Additional lua configuration, makes nvim stuff amazing!
+			"folke/neodev.nvim",
+		},
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+	},
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+		lazy = false,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup({ options = { theme = "tokyonight" } })
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
+	{
+		"xiyaowong/telescope-emoji.nvim",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				highlight = { enable = true },
+				indent = { enable = true },
+				auto_install = true,
+			})
+		end,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
+	{
+		"stevearc/oil.nvim",
+		config = function()
+			require("oil").setup({ view_options = { show_hidden = true } })
+		end,
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 
-  {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {},
-  },
-  {
-    "ggandor/leap.nvim",
-    config = function()
-      require("leap").add_default_mappings()
-    end,
-  },
-  {
-    "karb94/neoscroll.nvim",
-    config = function()
-      require("neoscroll").setup({})
-    end,
-  },
-  {
-    "kylechui/nvim-surround",
-    version = "*",
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({})
-    end,
-  },
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {},
+	},
+	{
+		"ggandor/leap.nvim",
+		config = function()
+			require("leap").add_default_mappings()
+		end,
+	},
+	{
+		"karb94/neoscroll.nvim",
+		config = function()
+			require("neoscroll").setup({})
+		end,
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*",
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+	},
+	{
+		"mfussenegger/nvim-lint",
+	},
 })
 
 -- TELESCOPE CONFIGURATION
 local telescope = require("telescope")
 local ta = require("telescope.actions")
 telescope.setup({
-  defaults = {
-    layout_strategy = "vertical",
-    mappings = {
-      i = {
-        ["<C-k>"] = ta.move_selection_previous,
-        ["<C-j>"] = ta.move_selection_next,
-        ["<C-q>"] = ta.send_selected_to_qflist + ta.open_qflist,
-      },
-    },
-  },
+	defaults = {
+		layout_strategy = "vertical",
+		mappings = {
+			i = {
+				["<C-k>"] = ta.move_selection_previous,
+				["<C-j>"] = ta.move_selection_next,
+				["<C-q>"] = ta.send_selected_to_qflist + ta.open_qflist,
+			},
+		},
+	},
 })
 telescope.load_extension("ui-select")
 telescope.load_extension("fzf")
@@ -169,29 +177,29 @@ telescope.load_extension("emoji")
 -- LSP BORDERS
 local _border = "single"
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = _border,
+	border = _border,
 })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = _border,
+	border = _border,
 })
 vim.diagnostic.config({
-  float = { border = _border },
+	float = { border = _border },
 })
 
 -- NVIM METALS
 local api = vim.api
 local metals_config = require("metals").bare_config()
 metals_config.settings = {
-  excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+	excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
 }
 metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 local nvim_metals_group = api.nvim_create_augroup("nvim-metals", { clear = true })
 api.nvim_create_autocmd("FileType", {
-  pattern = { "scala", "sbt", "java" },
-  callback = function()
-    require("metals").initialize_or_attach(metals_config)
-  end,
-  group = nvim_metals_group,
+	pattern = { "scala", "sbt", "java" },
+	callback = function()
+		require("metals").initialize_or_attach(metals_config)
+	end,
+	group = nvim_metals_group,
 })
 
 -- CMP
@@ -200,48 +208,48 @@ local luasnip = require("luasnip")
 local lspkind = require("lspkind")
 require("luasnip/loaders/from_vscode").lazy_load()
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-k>"] = cmp.mapping.select_prev_item(), --previous suggestion
-    ["<C-j>"] = cmp.mapping.select_next_item(), --next suggestion
-    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-    ["<C-e>"] = cmp.mapping.abort(),        --close completion window
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
-  }),
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" }, -- lsp
-    { name = "luasnip" },  -- snippets
-    { name = "buffer" },   -- text within current buffer
-    { name = "path" },     -- file system paths
-  }),
-  formatting = {
-    format = lspkind.cmp_format({ maxwidth = 50, ellipsis_char = "..." }),
-  },
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-k>"] = cmp.mapping.select_prev_item(), --previous suggestion
+		["<C-j>"] = cmp.mapping.select_next_item(), --next suggestion
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+		["<C-e>"] = cmp.mapping.abort(), --close completion window
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" }, -- lsp
+		{ name = "luasnip" }, -- snippets
+		{ name = "buffer" }, -- text within current buffer
+		{ name = "path" }, -- file system paths
+	}),
+	formatting = {
+		format = lspkind.cmp_format({ maxwidth = 50, ellipsis_char = "..." }),
+	},
 })
 
 -- MASON / LSP
 
 local servers = {
-  clangd = {},
-  pyright = {},
-  tsserver = {},
-  bashls = {},
-  jsonls = {},
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-    },
-  },
+	clangd = {},
+	pyright = {},
+	tsserver = {},
+	bashls = {},
+	jsonls = {},
+	lua_ls = {
+		Lua = {
+			workspace = { checkThirdParty = false },
+			telemetry = { enable = false },
+		},
+	},
 }
 
--- Setup neovim lua configuration
+-- lua setup!
 require("neodev").setup()
 
 -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
@@ -252,19 +260,48 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 local mason_lspconfig = require("mason-lspconfig")
 
 mason_lspconfig.setup({
-  ensure_installed = vim.tbl_keys(servers),
+	ensure_installed = vim.tbl_keys(servers),
 })
 
 mason_lspconfig.setup_handlers({
-  function(server_name)
-    require("lspconfig")[server_name].setup({
-      capabilities = capabilities,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    })
-  end,
+	function(server_name)
+		require("lspconfig")[server_name].setup({
+			capabilities = capabilities,
+			settings = servers[server_name],
+			filetypes = (servers[server_name] or {}).filetypes,
+		})
+	end,
 })
 
+-- LINTING
+local lint = require("lint")
+lint.linters_by_ft = {
+	python = { "pylint" },
+	cpp = { "cpplint" },
+}
+local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+	group = lint_augroup,
+	callback = function()
+		lint.try_lint()
+	end,
+})
+
+-- FORMATTING
+require("conform").setup({
+	formatters_by_ft = {
+		python = { "black" },
+		cpp = { "clang_format" },
+		lua = { "stylua" },
+		bash = { "shfmt" },
+		scala = {}, -- fallback to LSP formatter
+	},
+	format_on_save = {
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 1000,
+	},
+})
 
 -- KEYMAPS
 local keymap = vim.keymap -- for conciseness
@@ -277,10 +314,10 @@ keymap.set("n", "fo", ":join<CR>")
 keymap.set("n", "<leader>cf", "<cmd>edit $MYVIMRC<CR>")
 
 -- windows plit
-keymap.set("n", "<leader>nh", ":nohl<CR>")  -- example /keymap and then clear highlight
-keymap.set("n", "<leader>sv", "<C-w>v")     -- split window vertically
-keymap.set("n", "<leader>ss", "<C-w>s")     -- slit window horizontal
-keymap.set("n", "<leader>se", "<C-w>=")     -- make split equal width
+keymap.set("n", "<leader>nh", ":nohl<CR>") -- example /keymap and then clear highlight
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>ss", "<C-w>s") -- slit window horizontal
+keymap.set("n", "<leader>se", "<C-w>=") -- make split equal width
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split
 
 keymap.set("n", "<leader>tt", "<cmd>TroubleToggle<cr>")
@@ -290,15 +327,21 @@ keymap.set("n", "<leader>tl", "<cmd>TroubleToggle loclist<cr>")
 keymap.set("n", "<leader>tq", "<cmd>TroubleToggle quickfix<cr>")
 keymap.set("n", "<leader>d", "<cmd>TroubleToggle lsp_definitions<cr>")
 keymap.set("n", "<leader>r", "<cmd>TroubleToggle lsp_references<cr>")
-keymap.set("n", "<leader>tn", function() require("trouble").next({ skip_groups = true, jump = true }) end)
-keymap.set("n", "<leader>tp", function() require("trouble").previous({ skip_groups = true, jump = true }) end)
+keymap.set("n", "<leader>tn", function()
+	require("trouble").next({ skip_groups = true, jump = true })
+end)
+keymap.set("n", "<leader>tp", function()
+	require("trouble").previous({ skip_groups = true, jump = true })
+end)
 keymap.set("n", "<leader>h", vim.lsp.buf.hover)
 keymap.set("n", "<leader>i", vim.lsp.buf.implementation)
 keymap.set("n", "<leader>s", vim.lsp.buf.document_symbol)
 keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol)
 keymap.set("n", "<leader>sh", vim.lsp.buf.signature_help)
 keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
-keymap.set("n", "<leader>f", vim.lsp.buf.format)
+keymap.set("n", "<leader>f", function()
+	require("conform").format({ lsp_fallback = true, async = false, timeout_ms = 3000 })
+end)
 
 -- telescope
 local tb = require("telescope.builtin")
@@ -316,12 +359,16 @@ keymap.set("n", "<leader>co", tb.commands)
 keymap.set("n", "<leader>ts", tb.treesitter)
 keymap.set("n", "<leader>gf", tb.git_files)
 keymap.set("n", "<leader>mc", require("telescope").extensions.metals.commands)
-keymap.set("n", "<leader>cs", function() tb.colorscheme({ enable_preview = true }) end)
+keymap.set("n", "<leader>cs", function()
+	tb.colorscheme({ enable_preview = true })
+end)
 vim.api.nvim_set_keymap("n", "<leader>bb", ":Telescope file_browser<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<leader>em", ":Telescope emoji<CR>", { noremap = true })
 
 -- diagnostic in a float
-keymap.set("n", "<leader>fd", function() vim.diagnostic.open_float() end)
+keymap.set("n", "<leader>fd", function()
+	vim.diagnostic.open_float()
+end)
 
 -- todo
 keymap.set("n", "<leader>to", "<cmd>TodoTelescope<cr>", { silent = true, noremap = true })
@@ -359,4 +406,4 @@ opt.clipboard:append("unnamedplus")
 opt.splitright = true -- vertical split
 opt.splitbelow = true -- horizontal split
 
-vim.cmd "colorscheme tokyonight-storm"
+vim.cmd("colorscheme tokyonight-storm")
