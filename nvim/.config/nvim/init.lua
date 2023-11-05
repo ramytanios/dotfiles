2d43dad9aeed0e55fdaff196a2bd13ce412b6ecc
Ramy Tanios <ramy.tanios@gmail.com>
2023-11-05 21:27:24 +0100

    tweak oil config after update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -107,7 +107,12 @@ require("lazy").setup({
 			require("oil").setup({
 				keymaps = { ["<CR>"] = "actions.select" },
 				use_default_keymaps = false,
-				view_options = { show_hidden = true },
+				view_options = {
+					show_hidden = true,
+					is_always_hidden = function(name, _)
+						return name == ".."
+					end,
+				},
 			})
 		end,
 		-- Optional dependencies
